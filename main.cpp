9f49e547ff6f29a82a35d285c45b80aff2d29b1a
malav-mehta <malav.mehta@hotmail.com>
2020-05-30 12:41:19 -0400

    xcode

---
+++ main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  Competitive Programming
+//
+//  Created by Malav Mehta on 2020-05-30.
+//  Copyright © 2020 Malav Mehta. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
