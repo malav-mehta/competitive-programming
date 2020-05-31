1d64e3f24e7a0448b4aa8d46b83df0b500b15daf
malav-mehta <malav.mehta@hotmail.com>
2020-05-31 12:01:03 -0400

    cf 116a

---
+++ Codeforces/Tram.cpp
@@ -0,0 +1,18 @@
+#include <cstdio>
+using namespace std;
+
+int main() {
+    int n, a, b, max = 0, cur = 0;
+    scanf("%d", &n);
+    
+    while (n--) {
+        scanf("%d %d", &a, &b);
+        cur -= a;
+        cur += b;
+        if (cur > max)
+            max = cur;
+    }
+    
+    printf("%d", max);
+    return 0;
+}
