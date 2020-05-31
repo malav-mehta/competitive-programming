e3e9736ea9d8ad731965a27a6b62543d99101847
malav-mehta <malav.mehta@hotmail.com>
2020-05-31 11:10:19 -0400

    cf 144a

--- Codeforces/Arrival of the General.cpp
+++ Codeforces/Arrival of the General.cpp
@@ -1,15 +1,31 @@
-// TODO: This problem is incomplete.
-
 #include <cstdio>
 using namespace std;
 
 int main() {
-    int n;
+    int n, tmp, ans = 0;
     scanf("%d", &n);
-    int ar[n];
     
-    for (int i = 0; i < n; ++i)
-        scanf("%d", &ar[i]);
+    int mnN = 101, mnI = 0;
+    int mxN = 0, mxI = 0;
     
+    for (int i = 1; i <= n; ++i) {
+        scanf("%d", &tmp);
+        if (tmp > mxN) {
+            mxN = tmp;
+            mxI = i;
+        }
+        
+        if (tmp <= mnN) {
+            mnN = tmp;
+            mnI = i;
+        }
+    }
+    
+    ans = n - mnI + mxI - 1;
+    
+    if (mxI > mnI)
+        --ans;
+    
+    printf("%d", ans);
     return 0;
 }
