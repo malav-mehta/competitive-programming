f8c301ec115a0bc377d452c52ac3d5f638471784
malav-mehta <malav.mehta@hotmail.com>
2020-06-01 15:07:38 -0400

    cf 151a

---
+++ Codeforces/Soft Drinking.cpp
@@ -0,0 +1,17 @@
+#include <cstdio>
+using namespace std;
+
+int min(int a, int b) {
+    return a < b ? a : b;
+}
+
+int main() {
+    int n, j, l, c, d, p, nl, np;
+    scanf("%d %d %d %d %d %d %d %d", &n, &j, &l, &c, &d, &p, &nl, &np);
+    int mil = j * l / nl;
+    int sli = d * c;
+    int slt = p / np;
+    int ans = min(mil, min(sli, slt)) / n;
+    printf("%d", ans);
+    return 0;
+}
