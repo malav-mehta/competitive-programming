4c7a1fe0eaafb4cac6cbcfac82577a76ea823416
malav-mehta <malav.mehta@hotmail.com>
2020-06-03 12:41:18 -0400

    cf 205a

---
+++ Codeforces/Little Elephant and Rozdil.cpp
@@ -0,0 +1,23 @@
+#include <cstdio>
+using namespace std;
+
+int main() {
+    int n, mnN = 1000000001, mnI = 1, tmp;
+    scanf("%d", &n);
+    bool rep = false;
+    
+    for (int i = 1; i <=n; ++i) {
+        scanf("%d", &tmp);
+        if (tmp < mnN) {
+            rep = false;
+            mnN = tmp;
+            mnI = i;
+        }
+        
+        else if (tmp == mnN) rep = true;
+    }
+    
+    if (rep) printf("Still Rozdil");
+    else printf("%d", mnI);
+    return 0;
+}
