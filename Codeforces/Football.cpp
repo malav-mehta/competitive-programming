0f5a8791b5c661eb2afb6c725fc4ab82e864566b
malav-mehta <malav.mehta@hotmail.com>
2020-06-03 17:03:58 -0400

    cf 44a

---
+++ Codeforces/Football.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+#include <map>
+using namespace std;
+
+int main() {
+    int n;
+    string tmp;
+    map<string, int> mp;
+    cin >> n;
+    
+    while (n--) {
+        cin >> tmp;
+        if (mp.find(tmp) == mp.end())
+            mp.insert(make_pair(tmp, 1));
+        else
+            ++mp[tmp];
+    }
+    
+    
+    map<string, int>::iterator it;
+    int mxN = 0;
+    string winner;
+    
+    for (it = mp.begin(); it != mp.end(); ++it) {
+        if (it->second > mxN) {
+            winner = it->first;
+            mxN = it->second;
+        }
+    }
+    
+    cout << winner;
+    return 0;
+}
