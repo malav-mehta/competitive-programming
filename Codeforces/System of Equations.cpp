cd931b659ee6d86747c7d39efed2351578148c42
malav-mehta <malav.mehta@hotmail.com>
2020-06-06 13:29:00 -0400

    cf 214a

---
+++ Codeforces/System of Equations.cpp
@@ -0,0 +1,15 @@
+#include <cstdio>
+using namespace std;
+
+int main() {
+    int n, m, ans = 0;
+    scanf("%d %d", &n, &m);
+    
+    for (int a = 0; a * a <= n; ++a) {
+        int b = n - a * a;
+        if (a + b * b == m) ++ans;
+    }
+    
+    printf("%d\n", ans);
+    return 0;
+}
