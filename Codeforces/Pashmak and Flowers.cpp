cd060b37cb2b7d6e3b498a459c60ed49761ed958
malav-mehta <malav.mehta@hotmail.com>
2020-06-07 21:58:17 -0400

    added gitignore

---
+++ Codeforces/Pashmak and Flowers.cpp
@@ -0,0 +1,9 @@
+//
+//  Pashmak and Flowers.cpp
+//  Competitive Programming
+//
+//  Created by Malav Mehta on 2020-06-07.
+//  Copyright © 2020 Malav Mehta. All rights reserved.
+//
+
+#include <stdio.h>
