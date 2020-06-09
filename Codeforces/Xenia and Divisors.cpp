e087150c204f1979c803dd94c2d6d5d413fea4a2
malav-mehta <malav.mehta@hotmail.com>
2020-06-09 13:55:12 -0400

    cf 342a

--- Codeforces/Xenia and Divisors.cpp
+++ Codeforces/Xenia and Divisors.cpp
@@ -1,16 +1,35 @@
 #include <iostream>
+#include <cstring>
 using namespace std;
-
-int n;
-int s[99999];
 
 int main() {
     cin.sync_with_stdio(0);
     cin.tie(0);
     
+    int n, x, s[8];
+    memset(s, 0, sizeof(s));
     cin >> n;
-    for (int i = 0; i < n; ++i)
-        cin >> s[i];
+    
+    for (int i = 0; i < n; ++i) {
+        cin >> x;
+        ++s[x];
+    }
+    
+    if (!s[5] && !s[7] && s[2] >= s[4] && s[1] == s[2] + s[3] && s[1] == s[4] + s[6]) {
+        while (s[4]--) {
+            --s[2];
+            cout << "1 2 4\n";
+        }
+        
+        while (s[2]--)
+            cout << "1 2 6\n";
+        
+        while (s[3]--)
+            cout << "1 3 6\n";
+    }
+    
+    else
+        cout << -1;
     
     return 0;
 }
