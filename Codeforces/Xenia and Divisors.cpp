6a47f0adc4f36dbc4217d57e371e9317e653d2ed
malav-mehta <malav.mehta@hotmail.com>
2020-06-08 22:15:47 -0400

    started cf 342a

---
+++ Codeforces/Xenia and Divisors.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int n;
+int s[99999];
+
+int main() {
+    cin.sync_with_stdio(0);
+    cin.tie(0);
+    
+    cin >> n;
+    for (int i = 0; i < n; ++i)
+        cin >> s[i];
+    
+    return 0;
+}
