83f5c6251f2d33b29644b455055a8c41f47071e6
malav-mehta <malav.mehta@hotmail.com>
2020-06-10 19:29:40 -0400

    cf 258a

---
+++ Codeforces/Little Elephant and Bits.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    cin.sync_with_stdio(0);
+    cin.tie(0);
+    
+    string a;
+    bool f(0);
+    cin >> a;
+    
+    for (int i = 0; i < a.length() - 1; ++i) {
+        if (!f && a[i] == '0') f = 1;
+        else cout << a[i];
+    }
+    
+    if (f) cout << a[a.length() - 1];
+    return 0;
+}
