0757535b1a3d14b77696333b9b071f7b5127677d
malav-mehta <malav.mehta@hotmail.com>
2020-06-10 20:13:04 -0400

    started ccc

---
+++ CCC/00_J1_Calendar.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+#include <iomanip>
+using namespace std;
+
+int main() {
+    cin.sync_with_stdio(0);
+    cin.tie(0);
+    
+    int n, m, x;
+    bool done(0);
+    
+    cin >> n >> m;
+    cout << "Sun Mon Tue Wed Thr Fri Sat\n";
+    
+    for (int i = 0; i < n - 1; ++i)
+        cout << "    ";
+    
+    for (int i = n; i < n + m; ++i) {
+        x = i - n + 1;
+        cout << setw(3) << x;
+        if (i % 7 == 0) {
+            cout << "\n";
+            done = i == n + m - 1;
+        }
+        
+        else if (i != n + m - 1)
+            cout << " ";
+    }
+    
+    if (!done) cout << "\n";
+    return 0;
+}
