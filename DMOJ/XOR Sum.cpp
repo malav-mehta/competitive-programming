8e5471a9281ec72dcf16fe3d1de41e4cd16aecc8
malav-mehta <malav.mehta@hotmail.com>
2020-06-11 19:42:10 -0400

    dmoj/xor sum

---
+++ DMOJ/XOR Sum.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+typedef long long ll;
+
+int main() {
+    cin.sync_with_stdio(0);
+    cin.tie(0);
+    
+    ll n, a(0), b(0), x;
+    cin >> n;
+    
+    while (n--) {
+        cin >> x;
+        a += x;
+        b ^= x;
+    }
+    
+    cout << a - b << "\n";
+    return 0;
+}
