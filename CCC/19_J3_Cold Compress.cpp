22dc98a16e92fcc92e6f90f268a9bf7f07733ec6
malav-mehta <malav.mehta@hotmail.com>
2020-06-12 19:40:18 -0400

    ccc19j3

--- CCC/19_J3_Cold Compress.cpp
+++ CCC/19_J3_Cold Compress.cpp
@@ -1,9 +1,28 @@
-//
-//  19_J3_Cold Compress.cpp
-//  Competitive Programming
-//
-//  Created by Malav Mehta on 2020-06-12.
-//  Copyright © 2020 Malav Mehta. All rights reserved.
-//
+#include <iostream>
+using namespace std;
 
-#include <stdio.h>
+int main() {
+    cin.sync_with_stdio(0);
+    cin.tie(0);
+    
+    int n;
+    cin >> n;
+    
+    while (n--) {
+        string s;
+        cin >> s;
+        int cnt(1);
+        
+        for (int i = 1; i < (int) s.length(); ++i) {
+            if (s[i] == s[i - 1]) ++cnt;
+            else {
+                cout << cnt << " " << s[i - 1] << " ";
+                cnt = 1;
+            }
+        }
+        
+        cout << cnt << " " << s[s.length() - 1] << " ";
+        cout << "\n";
+    }
+    return 0;
+}
