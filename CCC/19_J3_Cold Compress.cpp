2ebc89aedd3a058031b7ae776d9fd48a81b0d85a
malav-mehta <malav.mehta@hotmail.com>
2020-06-12 19:30:20 -0400

    ccc19j2

---
+++ CCC/19_J3_Cold Compress.cpp
@@ -0,0 +1,9 @@
+//
+//  19_J3_Cold Compress.cpp
+//  Competitive Programming
+//
+//  Created by Malav Mehta on 2020-06-12.
+//  Copyright © 2020 Malav Mehta. All rights reserved.
+//
+
+#include <stdio.h>
