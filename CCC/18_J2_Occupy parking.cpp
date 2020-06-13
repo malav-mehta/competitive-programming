d5b33302788cde52af3b7d697d9676f7a72ceb9e
malav-mehta <malav.mehta@hotmail.com>
2020-06-12 20:52:25 -0400

    ccc18j2

---
+++ CCC/18_J2_Occupy parking.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    cin.sync_with_stdio(0);
+    cin.tie(0);
+        
+    int n, p[100], ans(0);
+    char c;
+    cin >> n;
+    
+    for (int i = 0; i < n; ++i) {
+        cin >> c;
+        if (c == 'C') p[i] = 1;
+        else p[i] = 0;
+    }
+    
+    for (int i = 0; i < n; ++i) {
+        cin >> c;
+        if (c == 'C' && p[i]) ++ans;
+    }
+    
+    cout << ans;
+    return 0;
+    
+}
