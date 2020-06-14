4629623e6b2c7310abf6d4a69a8a2939b885aa40
malav-mehta <malav.mehta@hotmail.com>
2020-06-14 14:38:10 -0400

    ccc15j2

---
+++ CCC/15_J2_Happy or Sad.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+#include <cstdio>
+#include <string>
+using namespace std;
+
+int substr_cnt(string str, string sub) {
+    int cnt(0);
+    size_t pos = str.find(sub, 0);
+
+    while (pos != string::npos) {
+        ++cnt;
+        pos = str.find(sub, pos + sub.length());
+    }
+    
+    return cnt;
+}
+
+int main() {
+    cin.sync_with_stdio(0);
+    cin.tie(0);
+    
+    string s;
+    getline(cin, s);
+    
+    int happy(substr_cnt(s, ":-)"));
+    int sad(substr_cnt(s, ":-("));
+
+    if (happy == 0 && sad == 0) printf("none");
+    else if (happy == sad) printf("unsure");
+    else if (happy > sad) printf("happy");
+    else printf("sad");
+    
+    return 0;
+}
