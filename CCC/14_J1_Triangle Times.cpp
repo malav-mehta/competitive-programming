bafb3778451412cf0d045ed7c2f48f32b8197538
malav-mehta <malav.mehta@hotmail.com>
2020-06-17 12:00:00 -0500

    ccc14j1

---
+++ CCC/14_J1_Triangle Times.cpp
@@ -0,0 +1,15 @@
+#include <cstdio>
+
+int main() {
+    int a, b, c;
+    scanf("%d %d %d", &a, &b, &c);
+    
+    if (a == b && b == c && c == 60) printf("Equilateral");
+    else if (a + b + c == 180 && (a == b || b == c || a == c)) printf("Isosceles");
+    else if (a + b + c == 180)
+        printf("Scalene");
+    else
+        printf("Error");
+    
+    return 0;
+}
