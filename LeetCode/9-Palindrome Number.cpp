c055468eb5c302b478d319032e6a860fda4f899a
malav-mehta <malav.mehta@hotmail.com>
2020-06-22 12:00:00 -0500

    leetcode #9

---
+++ LeetCode/9-Palindrome Number.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+class Solution {
+public:
+    bool isPalindrome(int x) {
+        string num = to_string(x);
+        string rev;
+        
+        for (int i = num.size() - 1; i >= 0; --i)
+            rev += num[i];
+        
+        return num == rev;
+    }
+};
