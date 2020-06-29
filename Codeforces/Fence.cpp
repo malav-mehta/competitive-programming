88aec6a2ef9e56b4d7233bfffd0f55e2e236553b
malav-mehta <malav.mehta@hotmail.com>
2020-06-29 01:12:14 -0400

    363b

--- Codeforces/Fence.cpp
+++ Codeforces/Fence.cpp
@@ -1,7 +1,28 @@
 #include <cstdio>
-using namespace std;
+
+const int mxN = 150000;
+int min, ans(1), tmp, n, k, h[mxN];
 
 int main() {
+    scanf("%d %d", &n, &k);
     
+    for (int i = 0; i < n; ++i)
+        scanf(" %d", &h[i]);
+    
+    tmp = 0;
+    for (int i = 0; i < k; ++i)
+        tmp += h[i];
+    min = tmp;
+    
+    for (int i = k; i < n; ++i) {
+        tmp += h[i];
+        tmp -= h[i - k];
+        if (tmp < min) {
+            ans = i - k + 2;
+            min = tmp;
+        }
+    }
+    
+    printf("%d", ans);
     return 0;
 }
