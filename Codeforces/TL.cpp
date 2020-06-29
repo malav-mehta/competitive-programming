b44e307f3f8277e1c12355edea7d76ecf6efa1b6
malav-mehta <malav.mehta@hotmail.com>
2020-06-29 18:27:14 -0400

    cf 350a

---
+++ Codeforces/TL.cpp
@@ -0,0 +1,32 @@
+#include <cstdio>
+#include <algorithm>
+using namespace std;
+
+int n, m;
+int a[100], b[100];
+
+int main() {
+    scanf("%d %d", &n, &m);
+    for (int i = 0; i < n; ++i)
+        scanf(" %d", &a[i]);
+    for (int i = 0; i < m; ++i)
+        scanf(" %d", &b[i]);
+    
+    sort(a, a+n);
+    sort(b, b+m);
+    
+    if (a[n-1] > b[0]) {
+        printf("-1");
+        return 0;
+    }
+    
+    for (int i = a[n-1]; i < b[0]; ++i) {
+        if (2 * a[0] <= i) {
+            printf("%d", i);
+            return 0;
+        }
+    }
+    
+    printf("-1");
+    return 0;
+}
