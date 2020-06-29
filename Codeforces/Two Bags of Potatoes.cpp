5e8f838bdfb2c0ddaf061de1bb2f13518ee09c3f
malav-mehta <malav.mehta@hotmail.com>
2020-06-29 19:03:45 -0400

    cf 239a

---
+++ Codeforces/Two Bags of Potatoes.cpp
@@ -0,0 +1,15 @@
+#include <cstdio>
+
+int main() {
+    int y, k, n;
+    scanf("%d %d %d", &y, &k, &n);
+    int x = k - y % k;
+    
+    if (x > n - y) printf("-1");
+    else while (x <= n - y) {
+        printf("%d ", x);
+        x += k;
+    }
+    
+    return 0;
+}
