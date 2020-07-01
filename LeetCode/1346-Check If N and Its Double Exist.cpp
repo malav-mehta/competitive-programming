b543ea160d9200fe857fbe27872ac295e4a0bb64
malav-mehta <malav.mehta@hotmail.com>
2020-06-30 21:29:23 -0400

    leetcode p1346

---
+++ LeetCode/1346-Check If N and Its Double Exist.cpp
@@ -0,0 +1,12 @@
+class Solution {
+public:
+    bool checkIfExist(vector<int>& arr) {
+        unordered_set<int> s;
+        for (int n : arr) {
+            if (s.find(n * 2) != s.end() || (n % 2 == 0 && s.find(n / 2) != s.end()))
+                return true;
+            s.insert(n);
+        }
+        return false;
+    }
+};
