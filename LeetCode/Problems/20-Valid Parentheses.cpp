2901e70d415ce6057b124205480129d31199ab24
malav-mehta <malav.mehta@hotmail.com>
2020-07-08 16:31:09 -0400

    leetcode p20

--- LeetCode/Problems/20-Valid Parentheses.cpp
+++ LeetCode/Problems/20-Valid Parentheses.cpp
@@ -1,36 +1,30 @@
-#include <iostream>
-#include <string>
-#include <stack>
-#include <unordered_map>
-using namespace std;
+/* 20. Valid Parentheses, 0 ms */
 
 class Solution {
 public:
     bool isValid(string s) {
-        stack<char> stk;
-        unordered_map<char, char> mp;
-        mp.insert(make_pair(')', '('));
-        mp.insert(make_pair(']', '['));
-        mp.insert(make_pair('}', '{'));
+        /*
+        1) Iterate through the string to check for opening parentheses, and
+        push the corresponding closing parenthese to the stack.
+        2) If the current character is not an opening parenthese, and it does
+        not match the closing parenthesese at the top of the stack, then the
+        structure is not valid.
+        3) At the end, the stack should be empty.
+        
+        Time complexity: O(n)
+        Space complexity: O(n)
+        */
+        
+        stack<char> st;
         
         for (char c : s) {
-            if (mp.find(c) != mp.end()) {
-                char top;
-                
-                if (stk.empty()) top = '\0';
-                else {
-                    top = stk.top();
-                    stk.pop();
-                }
-                
-                if (mp[c] != top) return false;
-            }
-            
-            else {
-                stk.push(c);
-            }
+            if (c == '(') st.push(')');
+            else if (c == '[') st.push(']');
+            else if (c == '{') st.push('}');
+            else if (st.empty() || c != st.top()) return false;
+            else st.pop();
         }
         
-        return stk.empty();
+        return st.empty();
     }
 };
