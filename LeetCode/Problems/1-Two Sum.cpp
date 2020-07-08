2b9eb0ae3a7b3c48837d4def6ee4938756fc808a
malav-mehta <malav.mehta@hotmail.com>
2020-07-08 14:57:02 -0400

    leetcode p1

--- LeetCode/Problems/1-Two Sum.cpp
+++ LeetCode/Problems/1-Two Sum.cpp
@@ -1,27 +1,28 @@
-#include <iostream>
-#include <vector>
-#include <unordered_map>
-using namespace std;
+/* 1. Two Sum, 20 ms */
 
 class Solution {
 public:
     vector<int> twoSum(vector<int>& nums, int target) {
         unordered_map<int, int> mp;
-        vector<int> ans(2, 0);
-        int search;
+        
+        /*
+        Insert each element of the nums array into the unordered_map for O(1)
+        key lookup time. Iterate through the other elements to check if the 
+        complement exists in the map. When the complement is found, the indices
+        of the two complements are returned.
+        
+        Time complexity: O(n)
+        Space complexity: O(n)
+        */
         
         for (int i = 0; i < nums.size(); ++i) {
-            search = target - nums[i];
-            
-            if (mp.find(search) != mp.end()) {
-                ans[0] = mp[search];
-                ans[1] = i;
-                return ans;
+            if (mp.find(target - nums[i]) != mp.end()) {
+                return {mp[target - nums[i]], i};
             }
             
             mp[nums[i]] = i;
         }
         
-        return ans;
+        return {-1, -1};
     }
 };
