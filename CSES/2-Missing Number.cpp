4eb96fc02e2c85db5f942949b02068a30c30d5ac
malav-mehta <malav.mehta@hotmail.com>
2020-07-08 21:03:44 -0400

    cses p2

---
+++ CSES/2-Missing Number.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#define ll long long
+using namespace std;
+
+int main() {
+    ll n, sum = 0;
+    cin >> n;
+
+    for (int i = 1; i < n; ++i) {
+        int t; cin >> t;
+        sum += t;
+    }
+
+    cout << n * (n + 1) / 2 - sum;
+    return 0;
+}
