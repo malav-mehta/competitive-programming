b0b36d14ba7428143338880cf22d35ba488b4500
malav-mehta <malav.mehta@hotmail.com>
2020-07-09 15:57:01 -0400

    leetcode p771

--- LeetCode/Problems/771-Jewels and Stones.cpp
+++ LeetCode/Problems/771-Jewels and Stones.cpp
@@ -1,16 +1,20 @@
+/* 771. Jewels and Stones, 4 ms */
+
 class Solution {
 public:
     int numJewelsInStones(string J, string S) {
+        /*
+        Construct an unordered set in O(m) time with each jewel.
+        Pass through the stones array and check if the code of each stone exists
+        in the set.
+        
+        Time complexity: O(n + m)
+        Space complexity: O(m)
+        */
+        int ans(0);
         unordered_set<char> j;
-        int ans = 0;
-        
-        for (char c : J)
-            j.insert(c);
-        
-        for (char c: S)
-            if (j.find(c) != j.end())
-                ++ans;
-        
+        for (char c : J) j.insert(c);
+        for (char c : S) if (j.find(c) != j.end()) ++ans;
         return ans;
     }
 };
