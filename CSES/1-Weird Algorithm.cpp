66f877a434bddad4a4d3412371ea0e87e59eff3b
malav-mehta <malav.mehta@hotmail.com>
2020-07-08 20:50:36 -0400

    cses p1

---
+++ CSES/1-Weird Algorithm.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+#define ll long long
+
+int main() {
+    ll n;
+    cin >> n;
+    cout << n;
+    
+    while (n > 1) {
+        if (n % 2) n = 3 * n + 1;
+        else n /= 2;
+        cout << " " << n;
+    }
+
+    return 0;
+}
