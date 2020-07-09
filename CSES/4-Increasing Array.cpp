7b333b08b7d7aeed5581fed16e8a73b614eb057a
malav-mehta <malav.mehta@hotmail.com>
2020-07-08 21:18:29 -0400

    cses p4

---
+++ CSES/4-Increasing Array.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#define max(a, b) (a > b ? a : b)
+#define ll long long
+using namespace std;
+
+int main() {
+    ll n, p, c, ans(0);
+    cin >> n >> p;
+    for (ll i = 1; i < n; ++i) {
+        cin >> c;
+        ans += max(0, p - c);
+        p = max(p, c);
+    }
+    
+    cout << ans;
+    return 0;
+}
