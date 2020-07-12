775b4aa441ccb496e64a74a1a8f8a7c29f0b60b0
malav-mehta <malav.mehta@hotmail.com>
2020-07-12 12:00:00 -0500

    ccc97s2

---
+++ CCC/97_S2_Nasty Numbers.cpp
@@ -0,0 +1,57 @@
+#include <iostream>
+#include <algorithm>
+#include <cmath>
+#include <vector>
+#include <set>
+
+#define min(a, b) (a < b ? a : b)
+#define max(a, b) (a > b ? a : b)
+
+using namespace std;
+
+int main() {
+    int n;
+    cin  >> n;
+
+    while (n--) {
+        int m;
+        cin  >> m;
+        bool nasty = false;
+
+        set<int> factors;
+        set< pair<int, int> > divs;
+
+        for (int d = 1; d < m / 2 + 1; ++d) {
+            if (m % d == 0) {
+                int x = m / d;
+                pair<int, int> div = make_pair(min(x, d), max(x, d));
+
+                if (divs.find(div) != divs.end()) {
+                    continue;
+                }
+
+                else {
+                    divs.insert(div);
+                }
+
+                int sum = x + d;
+                int diff = abs(x - d);
+
+                if (factors.find(sum) != factors.end() || factors.find(diff) != factors.end()) {
+                    nasty = true;
+                    break;
+                }
+
+                else {
+                    factors.insert(sum);
+                    factors.insert(diff);
+                }
+            }
+        }
+
+        if (nasty) cout << m << " is nasty\n";
+        else cout << m << " is not nasty\n";
+    }
+
+    return  0;
+}
