84dffefaf01b0a0acebf7551ebc44b6ae1d0493b
malav-mehta <malav.mehta@hotmail.com>
2020-08-03 20:16:04 -0400

    e p6

---
+++ Project Euler/6.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int ans(0),s(0);
+    for (int i=1;i<101;++i){
+        ans-=i*i;
+        s+=i;
+    }
+
+    cout << s*s+ans;
+    return 0;
+}
