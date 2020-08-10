c76f3f1aa1f63a1d0a0ccd2b35fdc80b5610f7e2
malav-mehta <malav.mehta@hotmail.com>
2020-08-09 23:04:19 -0400

    469a

--- Codeforces/I Wanna Be the Guy.cpp
+++ Codeforces/I Wanna Be the Guy.cpp
@@ -1,9 +1,35 @@
 #include <iostream>
 using namespace std;
 
+const int mxN = 101;
+int g[mxN] ={};
+
 int main() {
     int n;
     cin >> n;
-        
+
+    int p;
+    cin >> p;
+    while (p--) {
+        int t;
+        cin >> t;
+        g[t] = 1;
+    }
+
+    int a;
+    cin >> a;
+    while (a--) {
+        int t;
+        cin >> t;
+        g[t] = 1;
+    }
+
+    for (int i = 1; i <= n; ++i)
+        if (g[i] == 0) {
+            cout << "Oh, my keyboard!";
+            return 0;
+        }
+
+    cout << "I become the guy.";
     return 0;
 }
