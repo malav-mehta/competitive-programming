8dc4427306060c7ac14a296f506c0282affcbe07
malav-mehta <malav.mehta@hotmail.com>
2020-08-13 12:00:00 -0500

    day 13

---
+++ LeetCode/Monthly Challenges/2020 August/13-Iterator for Combination.cpp
@@ -0,0 +1,25 @@
+class CombinationIterator {
+public:
+    int n;
+    vector<int> c;
+    
+    CombinationIterator(string characters, int combinationLength) {
+        n = combinationLength;
+        
+    }
+    
+    string next() {
+        
+    }
+    
+    bool hasNext() {
+        
+    }
+};
+
+/**
+ * Your CombinationIterator object will be instantiated and called as such:
+ * CombinationIterator* obj = new CombinationIterator(characters, combinationLength);
+ * string param_1 = obj->next();
+ * bool param_2 = obj->hasNext();
+ */
