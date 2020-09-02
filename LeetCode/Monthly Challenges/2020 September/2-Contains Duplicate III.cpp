5a90193695f9e17b1fed8cc5b0a8a1533db98d27
malav-mehta <malav.mehta@hotmail.com>
2020-09-02 19:29:03 -0400

    day 2

---
+++ LeetCode/Monthly Challenges/2020 September/2-Contains Duplicate III.cpp
@@ -0,0 +1,17 @@
+ass Solution {
+    public:
+            bool containsNearbyAlmostDuplicate(vector<int>& nums, int k, int t) {
+                        set<long long> s;
+                                
+                                for (int i = 0; i < nums.size(); ++i) {
+                                                if (i > k && i - k - 1 < nums.size())
+                                                                    s.erase(nums[i - k - 1]);
+                                                            
+                                                            auto p = s.lower_bound((long long) nums[i] - t);
+                                                                        if (p != s.cend() && *p - nums[i] <= t) return true;
+                                                                                    s.insert(nums[i]);
+                                                                                            }
+                                        
+                                        return false;
+                                            }
+};
